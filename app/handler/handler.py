3e809d47299faf4dbddfd2013efa5727337f9a4c
Your Name <you@example.com>
2024-08-20 21:27:09 +0700

    [ADD] Global exception handler

---
+++ app/handler/handler.py
@@ -0,0 +1,21 @@
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+async def custom_exception_handler(request: Request, exc: StarletteHTTPException):
+    if type(exc.detail) == dict:
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                **exc.detail,
+                "status_code": exc.status_code
+            },
+        )
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={
+                "message": "Resource unavailable",
+                "status_code": 404
+            },
+        )
