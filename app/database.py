a5a233ed5afc8ea4555625c2248c4243173c9be7
Your Name <you@example.com>
2024-08-20 21:26:29 +0700

    [ADD] Beanie and Mongodb config

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from app.config import env
+from app.models.player import Player
+from app.models.pet import Pet
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client = AsyncIOMotorClient(env.mongo_url)
+    database = client[env.database]
+    await init_beanie(database, document_models=[Player, Pet])
+    print("INFO:", "Database Connection established", sep="\t")
+    yield
--- app/server.py
+++ app/server.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.exceptions import HTTPException
 from dotenv import find_dotenv, load_dotenv
-
+from app.database import lifespan
+from app.routes.player import player_router
+from app.middlewares.TelegramMiddleware import TelegramCodeMiddleware
+from app.handler.handler import custom_exception_handler
 load_dotenv(find_dotenv())
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+app.add_exception_handler(HTTPException, custom_exception_handler)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
+
+app.add_middleware(TelegramCodeMiddleware)
+app.include_router(player_router)
+
+@app.get('/')
+def index():
+    return {'message': 'Hello, World!'}
